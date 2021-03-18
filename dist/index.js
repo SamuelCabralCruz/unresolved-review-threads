52a3dc2d9f8f056707c00e7e72db7566b3f537ba
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2021-03-18 12:05:41 -0400

    feature/additional-trigger (#39)

--- dist/index.js
+++ dist/index.js
@@ -6105,7 +6105,11 @@ var EventType;
     EventType["PULL_REQUEST_LABELED"] = "pull_request_labeled";
     EventType["PULL_REQUEST_UNLABELED"] = "pull_request_unlabeled";
     EventType["PULL_REQUEST_SYNCHRONIZE"] = "pull_request_synchronize";
-    EventType["PULL_REQUEST_REVIEW_COMMENT_CREATED"] = "pull_request_review_comment_created";
+    EventType["PULL_REQUEST_REVIEW_REQUESTED"] = "pull_request_review_requested";
+    EventType["PULL_REQUEST_REVIEW_REQUEST_REMOVED"] = "pull_request_review_request_removed";
+    EventType["PULL_REQUEST_REVIEW_SUBMITTED"] = "pull_request_review_submitted";
+    EventType["PULL_REQUEST_REVIEW_EDITED"] = "pull_request_review_edited";
+    EventType["PULL_REQUEST_REVIEW_DISMISSED"] = "pull_request_review_dismissed";
     EventType["PULL_REQUEST_REVIEW_COMMENT_EDITED"] = "pull_request_review_comment_edited";
     EventType["PULL_REQUEST_REVIEW_COMMENT_DELETED"] = "pull_request_review_comment_deleted";
 })(EventType = exports.EventType || (exports.EventType = {}));
@@ -6123,13 +6127,26 @@ const eventTypeFrom = (eventName, eventAction) => {
                     return EventType.PULL_REQUEST_UNLABELED;
                 case 'synchronize':
                     return EventType.PULL_REQUEST_SYNCHRONIZE;
+                case 'review_requested':
+                    return EventType.PULL_REQUEST_REVIEW_REQUESTED;
+                case 'review_request_removed':
+                    return EventType.PULL_REQUEST_REVIEW_REQUEST_REMOVED;
+                default:
+                    throw new invalidEventTypeError_1.InvalidEventTypeError(eventName, eventAction);
+            }
+        case 'pull_request_review':
+            switch (eventAction) {
+                case 'submitted':
+                    return EventType.PULL_REQUEST_REVIEW_SUBMITTED;
+                case 'edited':
+                    return EventType.PULL_REQUEST_REVIEW_EDITED;
+                case 'dismissed':
+                    return EventType.PULL_REQUEST_REVIEW_DISMISSED;
                 default:
                     throw new invalidEventTypeError_1.InvalidEventTypeError(eventName, eventAction);
             }
         case 'pull_request_review_comment':
             switch (eventAction) {
-                case 'created':
-                    return EventType.PULL_REQUEST_REVIEW_COMMENT_CREATED;
                 case 'edited':
                     return EventType.PULL_REQUEST_REVIEW_COMMENT_EDITED;
                 case 'deleted':
